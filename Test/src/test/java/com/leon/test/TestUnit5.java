66d01286e5159e7bbf26605bca08063a7dd4f6de
Viktor <lvvj@mail.ru>
2018-03-23 15:16:14 +0200

    - initial commit

---
+++ Test/src/test/java/com/leon/test/TestUnit5.java
@@ -0,0 +1,53 @@
+package com.leon.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TestUnit5 {
+
+	@ParameterizedTest
+	@ValueSource(strings = { "racecar", "radar", "able was I ere I saw elba" })
+	void palindromes(String candidate) {
+		assertTrue(!candidate.isEmpty());
+	}
+
+	@ParameterizedTest
+	@MethodSource("stringProvider")
+	void testWithSimpleMethodSource(String argument) {
+		assertNotNull(argument);
+	}
+
+	static Stream<String> stringProvider() {
+		return Stream.of("foo", "bar");
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void testWithSimpleMethodSourceHavingNoValue(String argument) {
+		assertNotNull(argument);
+	}
+
+	static Stream<String> testWithSimpleMethodSourceHavingNoValue() {
+		return Stream.of("foo", "bar");
+	}
+
+	@TestFactory
+	Collection<DynamicTest> dynamicTestsFromCollection() {
+		return Arrays.asList(
+				dynamicTest("1st dynamic test", () -> assertTrue(true)),
+				dynamicTest("2nd dynamic test", () -> assertEquals(4, 2 * 2)
+						));
+	}
+}
